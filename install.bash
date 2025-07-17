30948bbe9c5dc8e116ccf326d9a67b428496c966
Zifan Ying <yzzzf0@gmail.com>
2025-07-17 15:30:00 +0800

    1111

--- install.bash
+++ install.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
-source ~/.bashrc
+source "${HOME}/conda/etc/profile.d/conda.sh"
 conda deactivate
 conda create -n vlmaps python=3.8 -y  # or use virtualenv
 conda activate vlmaps
 pip install pip==22.3 setuptools==65.5.0 wheel==0.37.1
+pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 # ================
 
 pip install -r requirements.txt
